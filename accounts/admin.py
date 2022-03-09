130f48e6dcb5421d42697abb7134e4afd643504e
tolep_samat <samat.tolep@gmail.com>
2022-03-09 14:53:46 +0600

    Зарегистрировал новую модель в админку

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from accounts.models import Profile, Gender
+from accounts.models import Profile, Gender, UserFollowing
 
 
 class ProfileInline(admin.StackedInline):
     model = Profile
-    fields = ['avatar', 'about', 'phone', 'gender', 'followings']
+    fields = ['avatar', 'about', 'phone', 'gender']
 
 
 class ProfileAdmin(UserAdmin):
@@ -18,4 +18,4 @@ User = get_user_model()
 admin.site.unregister(User)
 admin.site.register(User, ProfileAdmin)
 admin.site.register(Gender)
-
+admin.site.register(UserFollowing)
