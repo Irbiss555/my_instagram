e03848ede07259d7d3e3abb98e5c974155d9d624
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-09 21:39:37 +0600

    Создал представление для аутентификации пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, resolve_url
 
 # Create your views here.
+from core import settings
+
+
+def index_view(request):
+    return render(request, 'index.html', {'text': 'Hello world'})
+
+
+class LoginUserView(LoginView):
+    redirect_authenticated_user = True
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['login_page'] = 'True'
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        return next_url or resolve_url(settings.LOGIN_REDIRECT_URL)
