7660c07f29ab7f0fcecd20aac780212655b16524
tolep_samat <samat.tolep@gmail.com>
2022-03-10 10:14:10 +0600

    Добавил ссылки страниц с добавлением комментария

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 
-from .views import PostListView, PostCreateView
+from .views import (
+    PostListView, PostCreateView,
+    CommentCreateView, CommentDeleteView,
+)
 
-urlpatterns = [
+post_urls = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/create/', PostCreateView.as_view(), name='post_create')
 ]
+
+comment_urls = [
+    path('post/<int:pk>/comment/create', CommentCreateView.as_view(), name='comment_create'),
+    path('post/comment/<int:pk>/delete', CommentDeleteView.as_view(), name='comment_delete'),
+]
+
+
+urlpatterns = post_urls
+urlpatterns += comment_urls
