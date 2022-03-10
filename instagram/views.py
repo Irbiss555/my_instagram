a2b17a6db65389f4748976b19c3163e442c8fee4
tolep_samat <samat.tolep@gmail.com>
2022-03-10 10:14:32 +0600

    Добавил представления для комментариев

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView
 
 from .forms import PostForm
 from .models import Post
@@ -18,3 +18,11 @@ class PostCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('post_list')
+
+
+class CommentCreateView(CreateView):
+    pass
+
+
+class CommentDeleteView(DeleteView):
+    pass
