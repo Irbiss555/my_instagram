ae70ed791d73073a44d1466364b8023fc64c4e4a
tolep_samat <samat.tolep@gmail.com>
2022-03-11 16:45:23 +0600

    Добавил счетчик сигнала

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,7 +4,6 @@ from instagram.views import (
     PostListView, PostCreateView,
     CommentCreateView, CommentDeleteView,
     PostDetailView, LikeAddView,
-    LikeDeleteView,
 )
 
 app_name = 'instagram'
