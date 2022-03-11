b6f01f0ab64ce97c5a7199eb5bbda6c4186a3fb6
Мейрамбеков аспарух <asparukh2001@gmail.com>
2022-03-11 17:20:37 +0600

    Сделал сигнал количества коментария

--- instagram/signals.py
+++ instagram/signals.py
@@ -1,5 +1,5 @@
 from django.db.models.signals import post_save, post_delete
-from instagram.models import Like, Post
+from instagram.models import Like, Post, Comment
 from django.dispatch import receiver
 
 
@@ -8,4 +8,12 @@ def add_like_post(sender, instance, created, **kwargs):
     if created:
         post = Post.objects.get(pk=instance.post.id)
         post.likes_total += 1
+        post.save()
+
+
+@receiver(signal=post_save, sender=Comment)
+def add_comment(sender, instance, created, **kwargs):
+    if created:
+        post = Post.objects.get(pk=instance.post.id)
+        post.comment_total += 1
         post.save()
