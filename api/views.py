e2c3bb6ad514c2449d718a4071d6cd1e38cc6488
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-08 03:14:41 +0600

    #2 Создал viewset для лайков

--- api/views.py
+++ api/views.py
@@ -1,11 +1,33 @@
 from rest_framework import viewsets
-from instagram.models import Post
-from api.serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated
+
+from api.permissions import IsUserPostOwner
+from instagram.models import Post, Like
+from api.serializers import PostSerializer, LikeSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            print(1)
+            return []
+        elif self.request.method == 'POST':
+            return [IsAuthenticated()]
+        elif self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsUserPostOwner()]
+        return super().get_permissions()
+
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
