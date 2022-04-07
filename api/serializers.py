f117b6d158cb4368465308dd8c47bd81e4670144
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-08 03:11:32 +0600

    #2 Подправил сериализатор

--- api/serializers.py
+++ api/serializers.py
@@ -2,25 +2,6 @@ from rest_framework import serializers
 from instagram.models import Post, Like
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = [
-            'id',
-            'user',
-            'image',
-            'description',
-            'liked_users',
-            'likes_total',
-            'comment_total',
-        ]
-        read_only_fields = [
-            'user',
-            'likes_total',
-            'comment_total',
-        ]
-
-
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
@@ -28,4 +9,28 @@ class LikeSerializer(serializers.ModelSerializer):
             'id',
             'post',
             'user'
+        ]
+        read_only_fields = ['user']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    post_likes = LikeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'user',
+            'image',
+            'description',
+            'post_likes',
+            'liked_users',
+            'likes_total',
+            'comment_total',
+
+        ]
+        read_only_fields = [
+            'user',
+            'likes_total',
+            'comment_total',
         ]
