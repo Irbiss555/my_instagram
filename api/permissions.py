d77ff7c4cd21d589ecba5db4dc963e51a82e3115
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-08 11:11:56 +0600

    #2 Добавил правило на запрет всех действий

--- api/permissions.py
+++ api/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsUserPostOwner(BasePermission):
+class IsUserOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user.id == request.user.id
+
+
+class DenyAny(BasePermission):
+    def has_permission(self, request, view):
+        return False
