07dcb3781da49e3d004b8677516c5d72c90a43c6
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-08 16:34:24 +0600

    #3 Реализовал удаление и добавление лайков у поста в представлении

--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated
+from rest_framework.views import APIView
 
 from api.permissions import IsUserOwner, DenyAny
 from instagram.models import Post, Like
@@ -37,4 +41,38 @@ class LikeViewSet(viewsets.ModelViewSet):
             return []
         elif self.request.method in ['POST', 'DELETE']:
             return [IsAuthenticated(), IsUserOwner()]
+        return super().get_permissions()
+
+
+class LikeAPIView(APIView):
+    post_obj = None
+    permission_classes = [IsAuthenticated]
+
+    def dispatch(self, request, *args, **kwargs):
+        self.post_obj = get_object_or_404(Post, pk=self.kwargs.get('post_pk'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        likes = self.post_obj.post_likes.all()
+        serializer = LikeSerializer(likes, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    def post(self, request, *args, **kwargs):
+        if self.request.user in self.post_obj.liked_users.all():
+            self.remove_like()
+        else:
+            self.add_like()
+        response = {'likes_total': self.post_obj.liked_users.count()}
+        return JsonResponse(response, safe=False)
+
+    def remove_like(self):
+        like = Like.objects.get(user=self.request.user, post=self.post_obj)
+        like.delete()
+
+    def add_like(self):
+        Like.objects.create(user=self.request.user, post=self.post_obj)
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            return []
         return super().get_permissions()
